6bbc89a9965ebdf4b5dfb3fd6a26f1fb0fb807bc
Kuldeep-Yadav-1 <kulyad123@gmail.com>
2025-05-15 13:17:37 +0530

    javascript start

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,10 @@
 console.log("<-----javascript start---->")
 console.log("Hello javascript")
+document.getElementById("date").innerHTML= Date();
+document.getElementById("change-5").innerHTML = "<h3 style='color: red ;  text-transform: uppercase;'>Hello Worlds this is from js file</h3>";
+alert("hello" );
+// document.getElementsByClassName("change-5").innerHTML = <h1 style='text-transform: uppercase;'>hello world</h1>
+
+console.log(5+6);
+
+// window.print();
