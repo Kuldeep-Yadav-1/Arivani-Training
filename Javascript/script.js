e6dfece2c38359584c432c73d38007ba2eb30d46
Kuldeep-Yadav-1 <kulyad123@gmail.com>
2025-05-18 20:32:26 +0530

    javascript start

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,9 +2,83 @@ console.log("<-----javascript start---->")
 console.log("Hello javascript")
 document.getElementById("date").innerHTML= Date();
 document.getElementById("change-5").innerHTML = "<h3 style='color: red ;  text-transform: uppercase;'>Hello Worlds this is from js file</h3>";
-alert("hello" );
+// alert("hello" );
 // document.getElementsByClassName("change-5").innerHTML = <h1 style='text-transform: uppercase;'>hello world</h1>
 
 console.log(5+6);
 
 // window.print();
+
+console.log(Date())
+
+// document.write("<------JS Statement-------->")
+console.log("Started the task of 16 May 2025")
+let x, y , z ;
+x = 4 ;
+y = 5 ;
+z = x + y ;
+console.log("the total of x + y is stored in z  :", z);
+
+let a, b , c ;
+a = 1 ;
+b = 5 ;
+c = a + b ;
+console.log("the total of a + b is stored in c  :", c);
+document.getElementById("para-1").innerHTML = c ;
+
+let a1, b2 , c3 ;
+a1 = 1 ;  b2 = 2 ; c3 = a1 + b2 ;
+console.log("the total of a1 + b2 is stored in c  :", c3);
+document.getElementById("para-2").innerHTML = c3 ;
+
+let name1 ="kuldeep" ;
+let name2 =   "kuldeep" ;
+document.getElementById("name1").innerHTML= name1 ;
+document.getElementById("name2").innerHTML= name2 ;
+
+document.getElementById("para-5").innerHTML= "this is the number fixed value:" + 10.5 ;
+document.getElementById("para-6").innerHTML= "this is the String fixed value: " + "Sandeep" ;
+
+let x3;
+x3=10
+document.getElementById("para-7").innerHTML = "this is the varible declared with let :" +x3 ;
+
+const y3=11;
+document.getElementById("para-8").innerHTML = "this is the varible declared with const which is declare & assign in same line " + y3 ;
+
+let x1= 11 ;
+document.getElementById("para-10").innerHTML = x1 ;
+
+let x2 =10 ;
+let y2 =8 ;
+let z1 = x2%y2 ;
+document.getElementById("para-9").innerHTML = "remainder operation value is 10 % 8 is :" + z1 ;
+
+document.getElementById("para-11").innerHTML = "the value of expration 10*5 is : " + 10*5 ;
+document.getElementById("para-12").innerHTML = "the value of expration string  is : " +"Kuldeep"+" "+"Yadav" ;
+
+let lastname = "Shukla" ;
+let lastName = "Yadav" ;
+document.getElementById("para-13").innerHTML = lastname;
+document.getElementById("para-14").innerHTML = lastName;
+
+console.log("Variables are Containers for Storing Data JavaScript Variables can be declared in 4 ways: 1.Automatically 2.Using var 3.Using let 4.Using const");
+x1= 1;
+y1= 2;
+z1= x1 + y1 ;
+console.log("Automatically :",z1)
+
+var x4= 3;
+var y4= 4;
+var z4= x4 + y4 ;
+console.log("Using var :",z4)
+
+let x5= 5;
+let y5= 5;
+let z5= x5 + y5 ;
+console.log("Using let :",z5)
+
+const x6= 10;
+const y6= 11;
+const z6= x6 + y6 ;
+console.log("Using const:",z6)
