c5b4d12f60f4c53fe036f15376a6283d75ec9c6c
Kuldeep-Yadav-1 <kulyad123@gmail.com>
2025-05-21 10:03:37 +0530

    js operator

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,66 +1,66 @@
-console.log("<-----javascript start---->")
-console.log("Hello javascript")
-document.getElementById("date").innerHTML= Date();
-document.getElementById("change-5").innerHTML = "<h3 style='color: red ;  text-transform: uppercase;'>Hello Worlds this is from js file</h3>";
-// alert("hello" );
-// document.getElementsByClassName("change-5").innerHTML = <h1 style='text-transform: uppercase;'>hello world</h1>
+// console.log("<-----javascript start---->")
+// console.log("Hello javascript")
+// document.getElementById("date").innerHTML= Date();
+// document.getElementById("change-5").innerHTML = "<h3 style='color: red ;  text-transform: uppercase;'>Hello Worlds this is from js file</h3>";
+// // alert("hello" );
+// // document.getElementsByClassName("change-5").innerHTML = <h1 style='text-transform: uppercase;'>hello world</h1>
 
-console.log(5+6);
+// console.log(5+6);
 
-// window.print();
+// // window.print();
 
-console.log(Date())
+// console.log(Date())
 
-// document.write("<------JS Statement-------->")
-console.log("Started the task of 16 May 2025")
-let x, y , z ;
-x = 4 ;
-y = 5 ;
-z = x + y ;
-console.log("the total of x + y is stored in z  :", z);
+// // document.write("<------JS Statement-------->")
+// console.log("Started the task of 16 May 2025")
+// let x, y , z ;
+// x = 4 ;
+// y = 5 ;
+// z = x + y ;
+// console.log("the total of x + y is stored in z  :", z);
 
-let a, b , c ;
-a = 1 ;
-b = 5 ;
-c = a + b ;
-console.log("the total of a + b is stored in c  :", c);
-document.getElementById("para-1").innerHTML = c ;
+// let a, b , c ;
+// a = 1 ;
+// b = 5 ;
+// c = a + b ;
+// console.log("the total of a + b is stored in c  :", c);
+// document.getElementById("para-1").innerHTML = c ;
 
-let a1, b2 , c3 ;
-a1 = 1 ;  b2 = 2 ; c3 = a1 + b2 ;
-console.log("the total of a1 + b2 is stored in c  :", c3);
-document.getElementById("para-2").innerHTML = c3 ;
+// let a1, b2 , c3 ;
+// a1 = 1 ;  b2 = 2 ; c3 = a1 + b2 ;
+// console.log("the total of a1 + b2 is stored in c  :", c3);
+// document.getElementById("para-2").innerHTML = c3 ;
 
-let name1 ="kuldeep" ;
-let name2 =   "kuldeep" ;
-document.getElementById("name1").innerHTML= name1 ;
-document.getElementById("name2").innerHTML= name2 ;
+// let name1 ="kuldeep" ;
+// let name2 =   "kuldeep" ;
+// document.getElementById("name1").innerHTML= name1 ;
+// document.getElementById("name2").innerHTML= name2 ;
 
-document.getElementById("para-5").innerHTML= "this is the number fixed value:" + 10.5 ;
-document.getElementById("para-6").innerHTML= "this is the String fixed value: " + "Sandeep" ;
+// document.getElementById("para-5").innerHTML= "this is the number fixed value:" + 10.5 ;
+// document.getElementById("para-6").innerHTML= "this is the String fixed value: " + "Sandeep" ;
 
-let x3;
-x3=10
-document.getElementById("para-7").innerHTML = "this is the varible declared with let :" +x3 ;
+// let x3;
+// x3=10
+// document.getElementById("para-7").innerHTML = "this is the varible declared with let :" +x3 ;
 
-const y3=11;
-document.getElementById("para-8").innerHTML = "this is the varible declared with const which is declare & assign in same line " + y3 ;
+// const y3=11;
+// document.getElementById("para-8").innerHTML = "this is the varible declared with const which is declare & assign in same line " + y3 ;
 
-let x1= 11 ;
-document.getElementById("para-10").innerHTML = x1 ;
+// let x1= 11 ;
+// document.getElementById("para-10").innerHTML = x1 ;
 
-let x2 =10 ;
-let y2 =8 ;
-let z1 = x2%y2 ;
-document.getElementById("para-9").innerHTML = "remainder operation value is 10 % 8 is :" + z1 ;
+// let x2 =10 ;
+// let y2 =8 ;
+// let z1 = x2%y2 ;
+// document.getElementById("para-9").innerHTML = "remainder operation value is 10 % 8 is :" + z1 ;
 
-document.getElementById("para-11").innerHTML = "the value of expration 10*5 is : " + 10*5 ;
-document.getElementById("para-12").innerHTML = "the value of expration string  is : " +"Kuldeep"+" "+"Yadav" ;
+// document.getElementById("para-11").innerHTML = "the value of expration 10*5 is : " + 10*5 ;
+// document.getElementById("para-12").innerHTML = "the value of expration string  is : " +"Kuldeep"+" "+"Yadav" ;
 
-let lastname = "Shukla" ;
-let lastName = "Yadav" ;
-document.getElementById("para-13").innerHTML = lastname;
-document.getElementById("para-14").innerHTML = lastName;
+// let lastname = "Shukla" ;
+// let lastName = "Yadav" ;
+// document.getElementById("para-13").innerHTML = lastname;
+// document.getElementById("para-14").innerHTML = lastName;
 
 console.log("Variables are Containers for Storing Data JavaScript Variables can be declared in 4 ways: 1.Automatically 2.Using var 3.Using let 4.Using const");
 x1= 1;
@@ -82,3 +82,269 @@ const x6= 10;
 const y6= 11;
 const z6= x6 + y6 ;
 console.log("Using const:",z6)
+
+var x7= 1 ,y7=2 ,z7=3 ;
+console.log("x7,y7,z7 :",x7,y7,z7);
+
+let x8= 4 ,y8=5 ,z8=6 ;
+console.log("x8,y8,z8 :",x8,y8,z8);
+
+const x9= 7 ,y9=8 ,z9=9 ;
+let z10=10 ;
+z10=11
+console.log("x9,y9,z9,z10 :",x9,y9,z9,z10);
+console.log("sum of x9,y9,z9,z10 :",x9+y9+z9+z10);
+console.log("'5'+1 + 2 :",'5'+1 + 2);
+console.log("5+1 + '2' :",5+1 + '2');
+console.log("5+'1' + 2 :",5+'1' + 2);
+
+console.log("let Block Scope start here");
+// let scope
+
+{
+    let x = 2 ;
+    console.log ("let x :", x)
+}
+// console.log ("let x can not used in outer scope :", x)
+
+// var scope
+{
+    var x1 = 3 ;
+    console.log ("var x1 :", x1)
+}
+console.log ("var x1 is used in outer scope :", x1)
+
+let y10 = "kuldeep" ;
+// let y10 = 10 ; 
+// here it showes error b/c we cannot redeclare it again in the case of let .
+console.log("y10 :",y10);
+
+var x10 = "kuldeep" ;
+var x10 = 10 ; 
+// here it showes not error b/c we can redeclare it again in the case of var .
+console.log("x10 :", x10)
+
+
+var x11 =10 ;
+console.log("x11 :", x11)
+// here x is 10
+{
+    var x11 =9 ;
+    console.log("x11 :", x11)
+    // here x is 9
+}
+console.log("x11 :", x11)
+// here x is 9
+
+
+let y12 =10 ;
+console.log("y12 :", y12)
+// here y12 is 10
+{
+    let y12 =9 ;
+    console.log("y12 :", y12)
+    // here y12 is 9
+}
+console.log("y12 :", y12)
+// here y12 is 9
+
+// we start const varibale here
+
+const x12 = 3.14124 ;
+console.log("x12 : ", x12) ;
+// x12 =3.14
+// here it showes error because we cannot ressign again
+
+// const x13 ;
+// console.log(x13);
+// must be assigned
+// it showes error because it is necessory in the case of const we declare and reassign in the same line
+
+
+// Constant Objects and Arrays
+// The keyword const is a little misleading.
+// It does not define a constant value. It defines a constant reference to a value.
+
+const cars = ["suzuki" ,"toyota" , "hero" ,"mahindra" ];
+console.log("cars :", cars);
+cars[0] = "BMW"
+console.log("updated only an element of cars :", cars);
+
+// cars = ["ola" ,"volvo" , "audi" ,"honda" ];
+// console.log("again updated cars :", cars);
+// it showes type error
+
+const car ={
+    type : "xuv",
+    model : "700",
+    color : "black"
+}
+// we can update or replace the value in the case of Object.
+car.color ="silver" ;
+car.owner = "Anand Mahindra" ;
+// add the key owner
+console.log("car :", car);
+
+
+const x13 = 10 ;
+console.log("x13 :",x13);
+// here the value x13 is 10.
+
+{
+    const x13 =12
+    console.log("x13 :",x13);
+    // here the value x13 is 12.
+}
+console.log("x13 :",x13);
+// here the value x13 is 10.
+
+const x14 = 14;
+console.log("x14 :",x14);
+// here the value x14 is 14.
+{
+    const x14 =15 ;
+    console.log("x14 :",x14);
+    // here the value x14 is 15.
+}
+{
+    const x14 =13 ;
+    console.log("x14 :",x14);
+    // here the value x14 is 13.
+}
+
+carName = "Audi" ;
+var carName ;
+console.log("carName :" ,carName)
+
+// carName1 = "Audi" ;
+// let carName1 ;
+// console.log("carName1 :" ,carName1)
+// hoisting does not takes place in the case of let and const
+
+// carName2 = "Audi" ;
+// const carName2 ;
+// console.log("carName2 :" ,carName2)
+// hoisting does not takes place in the case of let and const
+
+console.log("JS operator start here")
+console.log("<--------Assignment operator---------->")
+let x =10 ;
+console.log("x : ",x);
+x+= x
+console.log("x+=x :", x)
+x-= x
+x =10 ;
+console.log("x-=x :", x)
+x*= x
+console.log("x*=x :", x)
+x/= x
+console.log("x/=x :", x)
+x%= x
+console.log("x%=x :", x)
+x=10 ; y=2
+console.log("x :",x,"y :",y )
+x**=2
+console.log("x**=y :", x)
+
+
+
+console.log("<------Arithmatic operator--------->")
+let x15 = 10 ;
+let y15 = 5 ;
+console.log(`the value of x15 is : ${x15} & y15 is : ${y15}`)
+console.log("Addition of x15 + y15" , x15 + y15);
+console.log("Subtraction of x15 - y15" , x15 - y15);
+console.log("Multiplication of x15 * y15" , x15 * y15);
+console.log("division of x15 / y15" , x15 / y15);
+console.log("Modulus of x15 % y15" , x15 % y15);
+console.log("Power of x15 ** y15" , x15 ** y15);
+x15++ ;
+console.log("X15++ " ,x15);
+y15-- ;
+console.log("y15-- " ,y15);
+
+console.log("<-----Comparision operator------>");
+let text1 = "A" ;
+let text2 = "B" ;
+console.log("text1 :", text1);
+console.log("text2 :", text2);
+console.log("text1 == text2 :", text1 == text2);
+console.log("text1 < text2 :", text1 < text2);
+console.log("text1 <= text2 :", text1 <= text2);
+console.log("text1 > text2 :", text1 > text2);
+console.log("text1 >= text2 :", text1 >= text2);
+console.log("text1 != text2 :", text1 != text2);
+text1 = 5 ;
+text2 = "5" ;
+console.log("text1 :", text1);
+console.log("text2 :", text2);
+console.log("text1 == text2 :", text1 == text2);
+console.log("text1 === text2 :", text1 === text2);
+console.log("text1 < text2 :", text1 < text2);
+console.log("text1 <= text2 :", text1 <= text2);
+console.log("text1 > text2 :", text1 > text2);
+console.log("text1 >= text2 :", text1 >= text2);
+console.log("text1 != text2 :", text1 != text2);
+
+console.log("<----Logical operator------>");
+text1 = 5 ;
+text2 = 6 ;
+let text3 = 7 ;
+let text4 = 8 ;
+console.log("text1 :", text1);
+console.log("text2 :", text2);
+console.log("text3 :", text3);
+console.log("text4 :", text4);
+console.log("<----Logical && operator------>");
+console.log("text1 >= text2 && text3 >= text4 :",text1 >= text2 && text3 >= text4);
+console.log("text1 >= text2 && text3 <= text4 :",text1 >= text2 && text3 <= text4);
+console.log("text1 <= text2 && text3 >= text4 :",text1 <= text2 && text3 >= text4);
+console.log("text1 <= text2 && text3 <= text4 :",text1 <= text2 && text3 <= text4);
+console.log("<----Logical || operator------>");
+console.log("text1 >= text2 || text3 >= text4 :",text1 >= text2 || text3 >= text4);
+console.log("text1 >= text2 || text3 <= text4 :",text1 >= text2 || text3 <= text4);
+console.log("text1 <= text2 || text3 >= text4 :",text1 <= text2 || text3 >= text4);
+console.log("text1 <= text2 || text3 <= text4 :",text1 <= text2 || text3 <= text4);
+console.log("<----Logical ! operator------>");
+console.log("text1 >= text2  :",text1 >= text2);
+console.log("!(text1 >= text2)   :",!(text1 >= text2));
+console.log("text3 <= text4 :",text3 <= text4);
+console.log("!(text3 <= text4) :",!(text3 <= text4));
+
+
+console.log("<----typeof operator------>");
+console.log("typeof('5') :",typeof('5') ) ;
+console.log("typeof(5) :",typeof(5) ) ;
+console.log("typeof(true) :",typeof(true) ) ;
+console.log("typeof(null) :",typeof(null) ) ;
+console.log("typeof(undefined) :",typeof(undefined) ) ;
+console.log("typeof([]) :",typeof([]) ) ;
+console.log("typeof({}) :",typeof({}) ) ;
+
+
+
+
+console.log("<-------- Terniary operator------>");
+text3 = 15 ;
+text4 = 10 ;
+console.log("text3 :", text3);
+console.log("text4 :", text4);
+let res = text3 < text4 ? "text3 is less than text4" : "text3 is greater than text4" ;
+console.log("result is :",res);
+
+console.log("<----String operator as a concatenation------>");
+let text5 = "Kuldeep" ;
+let text6 = "Yadav" ;
+console.log(text5 +" "+ text6);
+text5 += text6 ;
+console.log(text5);
+
+console.log("<----Adding string and no------>");
+console.log("'5' + 5 :",'5' + 5) ;
+console.log("'5' + '5' :",'5' + '5') ;
+console.log("5 + 5 :",5 + 5) ;
+console.log("'hello' + 5 :","hello" + 5) ;
+
+
+
+
