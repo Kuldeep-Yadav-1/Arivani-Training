8d0b727d0ba7e943ea23f493a979b90b8a757755
Kuldeep-Yadav-1 <kulyad123@gmail.com>
2025-06-04 19:52:23 +0530

    add validation on firstName password

--- Section 2/frontend/src/components/Form.jsx
+++ Section 2/frontend/src/components/Form.jsx
@@ -1,35 +1,120 @@
 import React, { useState } from "react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { Male, RemoveRedEye } from "@mui/icons-material";
+import { RemoveRedEye } from "@mui/icons-material";
 import { VisibilityOff } from "@mui/icons-material";
+import Swal from "sweetalert2";
+import { validateEmail } from "../utils/validateEmail";
+import profile from "./profile.jpg";
 
 function Form() {
   const [inputFirstName, setInputFirstName] = useState("");
   const [inputLastName, setInputLastName] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [inputPhone, setInputPhone] = useState("");
-  const [selctedGender, setSelectedGender] = useState("MALE");
+  const [selectedGender, setSelectedGender] = useState("MALE");
   const [selectDevice, setSelectDevice] = useState("");
   const [submit, setHandleSubmit] = useState(false);
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const handleSubmit = () => {
-    setHandleSubmit(true);
+  const handleSubmit = (e) => {
+    if (!inputFirstName.trim() || !inputEmail.trim() || !password.trim() || !confirmPassword.trim()) {
+      Swal.fire({
+        icon: "error",
+        title: "Caution",
+        text: "* fields are required.",
+        showConfirmButton: true,
+        timer: 3000,
+      });
+      return;
+    }
+
+    if (inputFirstName.length <= 3 || inputFirstName.length >= 30) {
+      Swal.fire({
+        icon: "error",
+        title: "Caution",
+        text: "First Name characters should be b/w 3 & 30",
+        showConfirmButton: true,
+        timer: 3000,
+      });
+      return;
+    }
+
+    if (!validateEmail(inputEmail)) {
+      Swal.fire({
+        icon: "error",
+        title: "Caution",
+        text: "Email is invalid",
+        showConfirmButton: true,
+        timer: 3000,
+      });
+      return;
+    }
+
+
+
+    if (password !== confirmPassword) {
+      // console.log("correct password") ;
+      // console.log("password :",password) ;
+      // console.log("confirm Password :",confirmPassword) ;
+      // alert("password & confirm password is not same");
+      Swal.fire({
+        icon: "error",
+        title: "Caution",
+        text: "Password & Confirm Password is Not Same",
+        showConfirmButton: true,
+        timer: 3000,
+      });
+      return;
+    } 
+
+    try {
+      if (inputFirstName.trim() && inputEmail.trim() && password.trim() && confirmPassword.trim()) {
+        // alert("First And Last name is required");
+        Swal.fire({
+          icon: "success",
+          title: "Signup Successfull",
+          showConfirmButton: true,
+          timer: 3000,
+        });
+      }
+      setHandleSubmit(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const toggledPasswordVisibility = () => {
+    setShowPassword((show) => !show);
+  };
+
+  const toggledConfirmPasswordVisibility = () => {
+    setShowConfirmPassword((show) => !show);
   };
 
   return (
-    <div className="my-15 p-5  w-[50%] mx-auto rounded shadow-2xl">
-      <form>
-        <h2 className="text-center text-bold text-2xl text-blue-900">
-          Login Details
-        </h2>
+    <div className="my-5 md:15 lg:my-15 p-5  w-[90%] md:w-[60%] lg:w-[50%] mx-auto rounded shadow-2xl">
+      <div>
         <div className="grid grid-cols-2 gap-2">
+          <div className="col-span-2 grid grid-cols-2">
+            <div className="mb-4 pe-2">
+              <img
+                src={profile}
+                className="w-[25%] lg:w-[22%]  ms-auto rounded-full border"
+              />
+            </div>
+            <div className="mb-4 py-[5%]">
+              <h2 className="text-left font-bold text-2xl">Sign Up</h2>
+            </div>
+          </div>
+
           <div className="">
-            <label className="" htmlFor="firstName">
-              First Name :
-            </label>{" "}
+            <label className="text-base font-medium" htmlFor="firstName">
+              First Name *
+            </label>
             <br />
             <input
               className="border mt-1 bg-white p-2 mb-5 rounded w-full"
@@ -37,13 +122,12 @@ function Form() {
               placeholder="Enter your First Name"
               value={inputFirstName}
               onChange={(e) => setInputFirstName(e.target.value)}
-              required
             />
           </div>
 
           <div className="">
-            <label className="" htmlFor="lastName">
-              Last Name :
+            <label className="text-base font-medium" htmlFor="lastName">
+              Last Name
             </label>{" "}
             <br />
             <input
@@ -56,7 +140,9 @@ function Form() {
           </div>
 
           <div className="col-span-2">
-            <label htmlFor="email">Email :</label>
+            <label htmlFor="email" className="text-base font-medium">
+              Email *
+            </label>
             <br />
             <input
               className="border mt-1 bg-white w-[100%] p-2 mb-5 rounded"
@@ -64,99 +150,26 @@ function Form() {
               placeholder="Enter your Email"
               value={inputEmail}
               onChange={(e) => setInputEmail(e.target.value)}
-              required
             />
           </div>
 
-          <div>
-            <label htmlFor="password">Password :</label>
-            <br />
-            <div className="relative">
-              <div>
-                <input
-                  className="border mt-1 bg-white w-[100%] p-2 mb-5 rounded"
-                  type="password"
-                  placeholder="Enter Password"
-                />
-              </div>
-              <div
-                className=""
-                style={{ position: "absolute", right: 10, top: 10 }}
-              >
-                <RemoveRedEye
-                  onClick={(e) => {
-                    console.log(e.relatedTarget.value);
-                    setPassword(e.target.value);
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <label htmlFor="password">Confirm Password :</label>
-            <br />
-            <div className="relative">
-              <div>
-                <input
-                  className="border mt-1 bg-white w-[100%] p-2 mb-5 rounded"
-                  type="password"
-                  placeholder="Enter Confirm Password"
-                />
-              </div>
-              <div
-                className=""
-                style={{ position: "absolute", right: 10, top: 10 }}
-              >
-                <VisibilityOff
-                  onClick={(e) => {
-                    console.log(e.relatedTarget.value);
-                    setConfirmPassword(e.target.value);
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="col-span-2">
-          <label htmlFor="device" className="pe-4 mt-1 mb-5">
-            Select Device :
-          </label>
-          <br />
-          <select
-            name="select your device"
-            id="device"
-            className="border p-2 mt-1 mb-5 rounded w-full"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setSelectDevice(e.target.value);
-            }}
-          >
-            <option value="phone" className="pe-2 mt-1 mb-5">
-              Phone
-            </option>
-            <option value="computer" className="pe-2 mt-1 mb-5" selected>
-              Computer
-            </option>
-          </select>
-        </div> */}
-
           <div className="col-span-2 ">
-            <label className="pe-4 mt-1 mb-5">Gender:</label>
+            <label className="pe-4 mt-1 mb-5 text-base font-medium">
+              Gender
+            </label>
             <input
               type="radio"
               id="male"
               name="gender"
               className="pe-2 mt-1 mb-5"
               value="MALE"
-              checked={selctedGender==="MALE"}
+              checked={selectedGender === "MALE"}
               onChange={(e) => {
                 console.log(e.target.value);
                 setSelectedGender(e.target.value);
               }}
-              selected
             />
-            <label htmlFor="male" className="pe-4 ms-1" >
+            <label htmlFor="male" className="pe-4 ms-1">
               Male
             </label>
             <input
@@ -165,13 +178,82 @@ function Form() {
               name="gender"
               className="pe-2 mt-1 mb-5"
               value="FEMALE"
-              checked={selctedGender==="FEMALE"}
               onChange={(e) => {
                 console.log(e.target.value);
                 setSelectedGender(e.target.value);
               }}
             />
-            <label htmlFor="female" className="ms-1">Female</label>
+            <label htmlFor="female" className="ms-1">
+              Female
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="text-base font-medium">
+              Password *
+            </label>
+            <br />
+            <div className="relative">
+              <div>
+                <input
+                  id="password"
+                  className="border mt-1 bg-white w-[100%] p-2 mb-5 rounded"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    console.log(e.target.value);
+                  }}
+                />
+              </div>
+              <div
+                className=""
+                style={{
+                  position: "absolute",
+                  right: 10,
+                  top: 12,
+                  cursor: "pointer",
+                }}
+                onClick={toggledPasswordVisibility}
+              >
+                {showPassword ? <RemoveRedEye /> : <VisibilityOff />}
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="confirmPassword" className="text-base font-medium">
+              Confirm Password *
+            </label>
+            <br />
+            <div className="relative">
+              <div>
+                <input
+                  id="confirmPassword"
+                  className="border mt-1 bg-white w-[100%] p-2 mb-5 rounded"
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Enter Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    console.log(e.target.value);
+                  }}
+                />
+              </div>
+              <div
+                className=""
+                style={{
+                  position: "absolute",
+                  right: 10,
+                  top: 12,
+                  cursor: "pointer",
+                }}
+                onClick={toggledConfirmPasswordVisibility}
+              >
+                {showConfirmPassword ? <RemoveRedEye /> : <VisibilityOff />}
+              </div>
+            </div>
           </div>
         </div>
 
@@ -181,37 +263,46 @@ function Form() {
             type="submit"
             onClick={handleSubmit}
           >
-            Submit
+            Sign In
           </button>
         </div>
 
         <div className="">
           {submit ? (
-            <div className="">
-              <div>
+            <div className="shadow-2xl  m-2 p-2">
+              <div className="md:w-[60%] lg:w-[40%] mx-auto mt-4">
                 <p>
-                  <strong>First Name :</strong>
+                  <strong>First Name* :</strong>
                   {inputFirstName}
                 </p>
                 <p>
-                  <strong>Last Name :</strong>
+                  <strong>Last Name : </strong>
                   {inputLastName}
                 </p>
                 <p>
-                  <strong>Email :</strong>
+                  <strong>Email* : </strong>
                   {inputEmail}
                 </p>
 
                 <p>
-                  <strong>Gender :</strong>
-                  {selctedGender}
+                  <strong>Gender : </strong>
+                  {selectedGender}
                 </p>
 
+                <p>
+                  <strong>Password* : </strong>
+                  {password}
+                </p>
+
+                <p>
+                  <strong>Confirm Password* : </strong>
+                  {confirmPassword}
+                </p>
               </div>
             </div>
-          ) : (<></>)}
+          ) : null}
         </div>
-      </form>
+      </div>
     </div>
   );
 }
