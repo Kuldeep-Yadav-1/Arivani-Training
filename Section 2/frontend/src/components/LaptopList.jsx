7fe95835cbf5681c7b9bb214bfcdc7b1575fcf18
Kuldeep-Yadav-1 <kulyad123@gmail.com>
2025-06-11 19:16:33 +0530

    up to date

--- Section 2/frontend/src/components/LaptopList.jsx
+++ Section 2/frontend/src/components/LaptopList.jsx
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from "react";
 import laptopData from "./laptopData";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import FilterListAltIcon from "@mui/icons-material/FilterListAlt";
+import Box from "@mui/material/Box";
 
 const LaptopList = () => {
   const [data, setData] = useState(laptopData);
   // const [search ,setSearch] = useState("")
 
-  const handleSearch = (e) =>{
+  const handleSearch = (e) => {
     // let search = (e.target.value).toLowerCase()
-    let fiteredData = laptopData.filter((item)=> item.model.toLowerCase().includes(e.target.value.toLowerCase()))
-    setData(fiteredData)
-  }
+    if (e.target.value.trim()) {
+      let fiteredData = data.filter((item) =>
+        item.model.toLowerCase().includes(e.target.value.trim().toLowerCase())
+      );
+      setData(fiteredData);
+    } else {
+      setData(laptopData);
+    }
+  };
+
+  const handleFilter = () => {
+    console.log("clicked");
+    return (
+      <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+        This Box renders as an HTML section element.
+      </Box>
+    );
+  };
 
   return (
     <div className="m-5">
-      <div className="relative">
-        <input className="border py-2 ps-9 rounded w-90" placeholder="Enter Model name"
-        // value={search}
-         onChange={handleSearch}/>
-        <div className="absolute left-2 top-2"><SearchIcon/> </div>
+      <div className="grid grid-cols-2 justify-between">
+        <div className="relative">
+          <input
+            className="border py-2 ps-9 rounded w-90"
+            placeholder="Enter Model name"
+            // value={search}
+            onChange={handleSearch}
+          />
+          <div className="absolute left-2 top-2">
+            <SearchIcon />
+          </div>
+        </div>
+        <div className="grid justify-end">
+          <button
+            className="bg-blue-700 px-5 py-2  rounded-lg text-white cursor-pointer"
+            onClick={handleFilter}
+          >
+            <FilterListAltIcon className="mr-1" />
+            Filter By
+          </button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {data.map((data, index) => {
